f1c462aeb72361cc24dd6ce415cef28862121791
namrata4447 <83541271+namrata4447@users.noreply.github.com>
2021-05-13 16:00:49 +0530

    updated new code app.java

--- src/main/java/com/namrata/app/App.java
+++ src/main/java/com/namrata/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!Learning jenkins and mevan" );
+        System.out.println( "Hello World!Learning jenkins,mevan and git" );
     }
 }
